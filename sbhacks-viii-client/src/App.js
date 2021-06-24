da3d373918f98124e2042fd5b76befd28418f1e0
Seif Ibrahim <seifibrahim10@gmail.com>
2021-06-24 00:54:32 -0700

    started stuff

---
+++ sbhacks-viii-client/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MailingList from "./components/MailingList.js";
+
+function App() {
+  return (
+    <div className="App">
+      <MailingList />
+    </div>
+  );
+}
+
+export default App;
---
+++ sbhacks-viii-client/src/components/MailingList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import axios from 'axios'
+
+function MailingList() {
+  const [emailAddress, setEmailAddress] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("/mailing-list", emailAddress).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const handleChange = (event) => {
+    setEmailAddress(event.target.value);
+  };
+
+  //useEffect(() => {}, [emailAddress]);
+  return (
+    <form noValidate onSubmit={handleSubmit}>
+      <TextField
+        id="email"
+        name="email"
+        type="email"
+        label="Email"
+        value={emailAddress}
+        placeholder="example@gmail.com"
+        onChange={handleChange}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        {" "}
+        Submit{" "}
+      </Button>
+    </form>
+  );
+}
+
+export default MailingList;
