cee59afc35992eff8e5f38474525ca3dc6546857
Seif Ibrahim <seifibrahim10@gmail.com>
2021-06-25 23:40:42 -0700

    es2021

--- sbhacks-viii-backend/functions/handlers/mailingList.js
+++ sbhacks-viii-backend/functions/handlers/mailingList.js
@@ -1,33 +1,33 @@
-const { admin, db } = require("../utils/admin");
-const { validateEmail } = require("../utils/mailingListUtils");
+const {admin, db} = require("../utils/admin");
+const {validateEmail} = require("../utils/mailingListUtils");
 const uuid = require("uuid");
 
 exports.mailingListSubscribe = async (req, res) => {
   const emailAddress = req.body.emailAddress;
   if (!validateEmail(emailAddress)) {
-    res.status(400).json({ error: `"${emailAddress}" is an invalid email` });
+    res.status(400).json({error: `"${emailAddress}" is an invalid email`});
     return;
   }
 
   const doc = await db
-    .collection("mailing-list-subscribers")
-    .doc(emailAddress)
-    .get();
+      .collection("mailing-list-subscribers")
+      .doc(emailAddress)
+      .get();
   if (doc.exists) {
-    res.status(400).json({ error: `"${emailAddress}" is already subscribed` });
+    res.status(400).json({error: `"${emailAddress}" is already subscribed`});
   }
 
   const subscriber = {
     "date-time-subscribed": admin.firestore.Timestamp.now(),
-    token: uuid.v4(),
+    "token": uuid.v4(),
   };
   db.collection("mailing-list-subscribers")
-    .doc(emailAddress)
-    .set(subscriber)
-    .then((doc) => {
-      res.json(subscriber);
-    })
-    .catch((err) => {
-      res.status(400).json({ error: `something went wrong: ${err}` });
-    });
+      .doc(emailAddress)
+      .set(subscriber)
+      .then((doc) => {
+        res.json(subscriber);
+      })
+      .catch((err) => {
+        res.status(400).json({error: `something went wrong: ${err}`});
+      });
 };
