83c1e37457c5398ae116daf893b4dc53f3488514
kyle <kyle_wong@ucsb.edu>
2021-08-31 00:46:11 -0700

    Implement Sponsors section with placeholders

--- sbhacks-viii-frontend/src/App.js
+++ sbhacks-viii-frontend/src/App.js
@@ -1,3 +1,6 @@
+import { hot } from "react-hot-loader/root";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 import MailingList from "./components/MailingList.js";
 import UnsubscribePage from "./components/UnsubscribePage.js";
@@ -5,12 +8,14 @@ import ConfirmPage from "./components/ConfirmPage.js";
 import { Switch, Route } from "react-router-dom";
 import SocialMediaBar from "./components/SocialMediaBar";
 import About from "./components/About.js";
+import Sponsors from "./components/Sponsors.js";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import NavBar from "./components/NavBar";
 
 import axios from "axios";
 
-axios.defaults.baseURL = "https://us-central1-sbhacks-viii-site.cloudfunctions.net/api";
+axios.defaults.baseURL =
+  "https://us-central1-sbhacks-viii-site.cloudfunctions.net/api";
 // axios.defaults.baseURL = "http://localhost:5001/sbhacks-viii-site/us-central1/api";
 
 const theme = createMuiTheme({
@@ -40,6 +45,7 @@ function App() {
             </div>
             <ThemeProvider theme={theme}>
               <About />
+              <Sponsors />
             </ThemeProvider>
           </div>
           <SocialMediaBar />
@@ -49,4 +55,4 @@ function App() {
   );
 }
 
-export default App;
+export default hot(App);
---
+++ sbhacks-viii-frontend/src/components/Sponsors.js
@@ -0,0 +1,201 @@
+import React from "react";
+import { makeStyles, Grid, Typography } from "@material-ui/core";
+
+import landmark from "../assets/images/landmark.svg"; // with import
+import firebase from "../assets/images/sponsors/firebase.png";
+import logmein from "../assets/images/sponsors/logmein.png";
+
+const useStyles = makeStyles((theme) => ({
+  landmark: {
+    width: 18,
+    paddingRight: 20,
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#043563",
+    padding: "0 10%",
+    paddingTop: "10vh",
+    textAlign: "center",
+  },
+  textContainer: {
+    textAlign: "left",
+    paddingLeft: 20,
+  },
+  textPrimary: {
+    fontFamily: "NexaBold",
+    fontSize: 34,
+  },
+  text: {
+    paddingTop: 30,
+    lineHeight: "250%",
+    fontFamily: "NexaLight",
+    fontSize: 24,
+  },
+  tier1Logo: {
+    width: "75%",
+  },
+  tier2Logo: {
+    width: "100%",
+  },
+  logoContainer: {
+    "&:hover": {
+      opacity: 0.7,
+    },
+  },
+  sponsorGrid: {
+    justifyContent: "center",
+  },
+  link: {
+    color: "#fab664",
+    "&:hover": {
+      opacity: 0.8,
+    },
+  },
+}));
+
+const Sponsors = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        variant="h4"
+        color="textPrimary"
+        className={classes.textPrimary}
+      >
+        <img src={landmark} className={classes.landmark} />
+        Sponsors
+      </Typography>
+
+      {/* Tier 1 */}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing={0}
+        className={classes.sponsorGrid}
+      >
+        <Grid item xs={12} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier1Logo} />
+          </a>
+        </Grid>
+      </Grid>
+
+      {/* Tier 2 */}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        className={classes.sponsorGrid}
+      >
+        <Grid item xs={6} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={6} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+      </Grid>
+
+      {/* Tier 3 */}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        className={classes.sponsorGrid}
+      >
+        <Grid item xs={3} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={3} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={3} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={3} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={3} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={3} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+      </Grid>
+
+      {/* Tier 4 */}
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        className={classes.sponsorGrid}
+      >
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+        <Grid item xs={2} className={classes.logoContainer}>
+          <a target="_blank" href="https://www.logmein.com/">
+            <img src={logmein} className={classes.tier2Logo} />
+          </a>
+        </Grid>
+      </Grid>
+
+      <Typography variant="h3" className={classes.text} color="textSecondary">
+        Interested in sponsoring? Contact us at{" "}
+        <a href="mailto:sponsors@sbhacks.com" className={classes.link}>
+          sponsors@sbhacks.com
+        </a>
+      </Typography>
+    </div>
+  );
+};
+
+export default Sponsors;
