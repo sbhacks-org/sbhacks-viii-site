a26dc75a34599e0ffa4c47a4944d434fda02cd40
dguo <dguo@umail.ucsb.edu>
2021-08-30 23:55:59 -0700

    backend for hacker authentication and application updates added... not yet tested

--- sbhacks-viii-backend/functions/index.js
+++ sbhacks-viii-backend/functions/index.js
@@ -10,6 +10,13 @@ const {
   getMailingListAddresses,
 } = require("./handlers/adminFeatures");
 
+const {
+  register,
+  login,
+  saveApp,
+  getAppFields,
+} = require("./handlers/userdb");
+
 const app = require("express")();
 
 const cors = require("cors")({origin: true});
@@ -20,6 +27,11 @@ app.get("/mailing-list/unsubscribe", mailingListUnsubscribe);
 app.get("/mailing-list/confirm", mailingListConfirm);
 
 app.get("/admin/mailingListAddresses", getMailingListAddresses);
+
+app.post("/userdb/register", register);
+app.post("/userdb/login", login);
+app.post("/userdb/saveApp", saveApp);
+app.get("/userdb/getAppFields", getAppFields);
 
 exports.api = functions.https.onRequest(app);
 
