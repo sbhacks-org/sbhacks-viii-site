9078611c276b8c865c19946a1453936ef11c61a9
dguo <dguo@umail.ucsb.edu>
2021-09-03 20:57:26 -0700

    added adminFeatures for mailing list to get confirmed email addresses

--- sbhacks-viii-backend/functions/index.js
+++ sbhacks-viii-backend/functions/index.js
@@ -6,6 +6,10 @@ const {
   mailingListConfirm,
 } = require("./handlers/mailingList");
 
+const {
+  getMailingListAddresses,
+} = require("./handlers/adminFeatures");
+
 const app = require("express")();
 
 const cors = require("cors")({origin: true});
@@ -15,4 +19,7 @@ app.post("/mailing-list/subscribe", mailingListSubscribe);
 app.get("/mailing-list/unsubscribe", mailingListUnsubscribe);
 app.get("/mailing-list/confirm", mailingListConfirm);
 
+app.get("/admin/mailingListAddresses", getMailingListAddresses);
+
 exports.api = functions.https.onRequest(app);
+
