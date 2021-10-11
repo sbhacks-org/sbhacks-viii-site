86bfebc9394c0cee4ca3b4a12472f3a36999811c
kyle <kyle_wong@ucsb.edu>
2021-10-11 12:00:41 -0700

    added styles to login/register page

--- sbhacks-viii-frontend/src/components/AuthenticationPage.js
+++ sbhacks-viii-frontend/src/components/AuthenticationPage.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { Link } from "react-router-dom";
+import {
+  makeStyles,
+  Grid,
+  Typography,
+  TextField,
+  FormControl,
+  FormLabel,
+} from "@material-ui/core";
 
 import {
   getAuth,
@@ -13,13 +21,95 @@ import {
 } from "firebase/auth";
 
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import "../styles/AuthenticationPage.css";
+
+import Background from "../assets/backgrounds/tileable_background.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundImage: `url(${Background})`,
+    height: "100%",
+  },
+  centerContainer: {
+    paddingTop: "80px",
+    margin: "0 auto",
+    [theme.breakpoints.up("md")]: {
+      width: "600px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "80vw",
+    },
+  },
+  titleContainer: {
+    textAlign: "left",
+    marginBottom: "30px",
+  },
+  formContainer: {
+    borderRadius: "0px 50px 50px 50px",
+    background: "#EEFBFF",
+    padding: "40px 10px",
+  },
+  textField: {
+    marginBottom: "40px",
+    [theme.breakpoints.up("md")]: {
+      width: "150%",
+      transform: "translate(-15%)",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      transform: "translate(0%)",
+    },
+  },
+  submitBtn: {
+    border: "none",
+    textAlign: "center",
+    padding: "15px 32px",
+    display: "inline-block",
+    color: "white",
+    background: "#2FA0DF",
+    borderRadius: "75px",
+    fontSize: 15,
+    width: "40%",
+    "&:hover": {
+      background: "#5FC5FF",
+    },
+    marginBottom: "40px",
+    marginTop: "20px",
+  },
+  loginTab: {
+    [theme.breakpoints.up("md")]: {
+      width: "200px",
+      height: "50px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "125px",
+      height: "40px",
+    },
+    width: "8vw",
+    height: "50px",
+    borderRadius: "35px 35px 0px 0px",
+    border: "none",
+  },
+  registerTab: {
+    [theme.breakpoints.up("md")]: {
+      width: "200px",
+      height: "50px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "125px",
+      height: "40px",
+    },
+    transform: "translateX(-10%)",
+    width: "8vw",
+    borderRadius: "35px 35px 0px 0px",
+    border: "none",
+  },
+}));
 
 const AuthenticationPage = (props) => {
-  useEffect(() => { }, []);
+  useEffect(() => {}, []);
+  const classes = useStyles();
   const [showLogin, setShowLogin] = useState(true);
   const [resume, setResume] = useState(undefined);
-  
 
   // might be helpful: https://firebase.google.com/docs/auth/web/manage-users#get_the_currently_signed-in_user
   useEffect(() => {
@@ -58,7 +148,7 @@ const AuthenticationPage = (props) => {
       });
   };
 
-  const registerSubmit = (e, email, password, fname, lname,) => {
+  const registerSubmit = (e, email, password, fname, lname) => {
     e.preventDefault();
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
@@ -77,7 +167,12 @@ const AuthenticationPage = (props) => {
         console.log(user.uid); // use this for identifying user in backend
 
         // TO DO: add to db
-        axios.post("/userdb/register", { uid: user.uid, emailAddress: email, fname: fname, lname: lname});
+        axios.post("/userdb/register", {
+          uid: user.uid,
+          emailAddress: email,
+          fname: fname,
+          lname: lname,
+        });
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -86,7 +181,6 @@ const AuthenticationPage = (props) => {
         // ..
       });
   };
-
 
   const update = (e, set) => {
     e.preventDefault();
@@ -118,41 +212,64 @@ const AuthenticationPage = (props) => {
   };
 
   return (
-    <div id='landingPage'>
-      <div className='title'>Welcome to SB Hacks VIII</div>
-      <div><button
-        onClick={() => {
-          setShowLogin(true);
-        }}
-      >
-        Login
-      </button>
-        <button
-          onClick={() => {
-            setShowLogin(false);
-          }}
-        >
-          Register
-        </button>
+    <div className={classes.container}>
+      <div className={classes.centerContainer}>
+        <div className={classes.titleContainer}>
+          <Typography color="secondary" variant="h4">
+            Welcome to SB Hacks VIII
+          </Typography>
+        </div>
+        <div style={{ textAlign: "left" }}>
+          <button
+            className={classes.loginTab}
+            style={{
+              background: showLogin ? "#EEFFFF" : "#C6E9F4",
+              position: "relative",
+              zIndex: showLogin ? 2 : 1,
+            }}
+            onClick={() => {
+              setShowLogin(true);
+            }}
+          >
+            <Typography variant="subtitle2" color="primary">
+              LOGIN
+            </Typography>
+          </button>
+          <button
+            className={classes.registerTab}
+            style={{
+              background: showLogin ? "#C6E9F4" : "#EEFFFF",
+              position: "relative",
+              zIndex: showLogin ? 1 : 2,
+            }}
+            onClick={() => {
+              setShowLogin(false);
+            }}
+          >
+            <Typography variant="subtitle2" color="primary">
+              REGISTER
+            </Typography>
+          </button>
+        </div>
+        <div className={classes.formContainer}>
+          {showLogin ? (
+            <>
+              <AuthenticationForm
+                askForName={false}
+                handlesubmit={loginSubmit}
+                submitTxt="LOG IN"
+              />
+              <Link to="resetpassword">Forgot your password?</Link>
+            </>
+          ) : (
+            <AuthenticationForm
+              askForName={true}
+              handlesubmit={registerSubmit}
+              submitTxt="REGISTER"
+            />
+          )}
+        </div>
       </div>
-      {showLogin ? (
-        <AuthenticationForm 
-          askForName={false}
-          handlesubmit={loginSubmit}
-          submitTxt="Login" />
-      ) : (
-        <AuthenticationForm
-          askForName={true}
-          handlesubmit={registerSubmit}
-          submitTxt="Register"
-        />
-      )}
-      <Link to='resetpassword'>Forgot your password?</Link>
-
-      {/* <form>
-        <input type="file" onChange={(e) => setResume(e.target.files[0])} />
-        <button onClick={(e) => uploadResume(e)}>Submit Resume</button>
-      </form> */}
     </div>
   );
 };
@@ -160,12 +277,14 @@ const AuthenticationPage = (props) => {
 export default AuthenticationPage;
 
 export const AuthenticationForm = (props) => {
+  const classes = useStyles();
+
   const handleSubmit = props.handlesubmit;
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [fname, setFname] = useState('');
-  const [lname, setLname] = useState('');
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
 
   const update = (e, set) => {
     e.preventDefault();
@@ -174,21 +293,72 @@ export const AuthenticationForm = (props) => {
 
   return (
     <form onSubmit={(e) => handleSubmit(e, email, password, fname, lname)}>
-      {
-        props.askForName &&
-        <>
-          <input type='text' value={fname} onChange={(e) => update(e, setFname)} />
-          <input type='text' value={lname} onChange={(e) => update(e, setLname)} />
-        </>
-      }
-      <input type="email" value={email} onChange={(e) => update(e, setEmail)} />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => update(e, setPassword)}
-      />
-
-      <button>{props.submitTxt}</button>
+      <div className={classes.inputContainer}>
+        {props.askForName && (
+          <>
+            <div>
+              <FormControl>
+                <TextField
+                  label="First name"
+                  name="fname"
+                  type="text"
+                  value={fname}
+                  onChange={(e) => update(e, setFname)}
+                  className={classes.textField}
+                  inputProps={{ style: { fontSize: 20 } }}
+                  InputLabelProps={{ style: { fontSize: 20 } }}
+                  fullWidth
+                />
+              </FormControl>
+            </div>
+            <div>
+              <FormControl>
+                <TextField
+                  label="Last name"
+                  name="lname"
+                  type="text"
+                  value={lname}
+                  onChange={(e) => update(e, setLname)}
+                  className={classes.textField}
+                  inputProps={{ style: { fontSize: 20 } }}
+                  InputLabelProps={{ style: { fontSize: 20 } }}
+                />
+              </FormControl>
+            </div>
+          </>
+        )}
+        <div>
+          <FormControl>
+            <TextField
+              label="Email"
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => update(e, setEmail)}
+              className={classes.textField}
+              inputProps={{ style: { fontSize: 20 } }}
+              InputLabelProps={{ style: { fontSize: 20 } }}
+            />
+          </FormControl>
+        </div>
+        <div>
+          <FormControl>
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => update(e, setPassword)}
+              className={classes.textField}
+              inputProps={{ style: { fontSize: 20 } }}
+              InputLabelProps={{ style: { fontSize: 20 } }}
+            />
+          </FormControl>
+        </div>
+      </div>
+      <button type="submit" className={classes.submitBtn}>
+        <Typography variant="subtitle1">{props.submitTxt}</Typography>
+      </button>
     </form>
   );
 };
--- sbhacks-viii-frontend/src/components/NavBar.js
+++ sbhacks-viii-frontend/src/components/NavBar.js
@@ -1,92 +1,165 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "../styles/NavBar.scss";
 
 import Close from "../assets/images/close_icon.png";
 import HamburgerIcon from "../assets/images/hamburger_menu_icon.png";
 
-const NavBar = () => {
-    const landingPageLink = 'https://sbhacks.com/';
-    const landing = '#landing'; //'#landing';
-    const intro = '#intro'; // '#intro';
-    const devpost = 'https://sb-hacks-vii.devpost.com/';
+const NavBar = (props) => {
+  const isLoggedIn = props.isLoggedIn;
 
-    const faq = ''; //'#faq';
-    const team = ''; //'#team';
-    const sponsors = '#sponsors';
-    const mentors = ''; //
-    const volunteer = ''; // 'volunteer';
-    const pastSite = ''; // 'https://sbhacksvii.com';
+  const landingPageLink = "https://sbhacks.com/";
+  const landing = "#landing"; //'#landing';
+  const intro = "#intro"; // '#intro';
+  const devpost = "https://sb-hacks-vii.devpost.com/";
 
-    const [open, setOpen] = useState(false);
+  const faq = ""; //'#faq';
+  const team = ""; //'#team';
+  const sponsors = "#sponsors";
+  const mentors = ""; //
+  const volunteer = ""; // 'volunteer';
+  const pastSite = ""; // 'https://sbhacksvii.com';
 
-    const toggleMobNavBar = () => {
-        console.log('toggle mobile navbar')
-        let navLinks = document.getElementById("navLinks");
-        if (navLinks.style.display === "block") {
-            navLinks.style.display = "none";
-            setOpen(false);
-        } else {
-            navLinks.style.display = "block";
-            setOpen(true)
-        }
+  const [open, setOpen] = useState(false);
+
+  const toggleMobNavBar = () => {
+    console.log("toggle mobile navbar");
+    let navLinks = document.getElementById("navLinks");
+    if (navLinks.style.display === "block") {
+      navLinks.style.display = "none";
+      setOpen(false);
+    } else {
+      navLinks.style.display = "block";
+      setOpen(true);
     }
-    // const checkIfMobile = () => {
-    //     if (window.innerWidth <= '768px') {
-    //         toggleMobNavBar();
-    //     }
-    // }
+  };
+  // const checkIfMobile = () => {
+  //     if (window.innerWidth <= '768px') {
+  //         toggleMobNavBar();
+  //     }
+  // }
 
-    // useEffect(() => {
-    //     window.addEventListener('resize', checkIfMobile);
-    //     return () => window.removeEventListener('resize', checkIfMobile)
-    // }, []);
+  // useEffect(() => {
+  //     window.addEventListener('resize', checkIfMobile);
+  //     return () => window.removeEventListener('resize', checkIfMobile)
+  // }, []);
 
-    // SHOULD Replace A Tags that are links with <Link/>
+  // SHOULD Replace A Tags that are links with <Link/>
+
+  if (isLoggedIn) {
     return (
-        <div id='navBarContainer'>
+      <div id="navBarContainer">
+        <div
+          className="mobNavBar"
+          style={{
+            backgroundColor: open
+              ? "var(--mobile-background-color)"
+              : "inherit",
+          }}
+        >
+          {/* <!-- Navigation links (hidden by default) --> */}
+          <div id="navLinks">
+            <a href={`${landing}`}>LOG OUT</a>
+          </div>
+          {/* <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links --> */}
+          <a
+            href="javascript:void(0);"
+            className="icon"
+            onClick={toggleMobNavBar}
+          >
+            {open ? (
+              <img className="mobileIcon" src={Close} />
+            ) : (
+              <img className="mobileIcon" src={HamburgerIcon} />
+            )}
+          </a>
+        </div>
 
-            <div className="mobNavBar" style={{backgroundColor: (open) ? 'var(--mobile-background-color)': 'inherit'}}>
-                {/* <!-- Navigation links (hidden by default) --> */}
-                <div id="navLinks">
-                    <a href={`${landing}`}>SB Hacks VIII</a>
-                    <a href={`${intro}`}>About Us</a>
-                    <a href={`${sponsors}`}>Sponsors</a>
-                    {/* <a href={`${faq}`}>FAQ</a>
+        <div id="navBar">
+          <ul>
+            {/* <li><Link className='last' href={`${landingPageLink}${pastSite}`}>SB HACKS VII</Link></li>
+                      <li><Link href={`${landingPageLink}${volunteer}`}>VOLUNTEER</Link></li>
+                      <li><Link href={`${landingPageLink}${mentors}`}>MENTOR</Link></li> */}
+            <li>
+              <a className="last" target="_blank" rel="noopener noreferrer">
+                LOG OUT
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div id="navBarContainer">
+        <div
+          className="mobNavBar"
+          style={{
+            backgroundColor: open
+              ? "var(--mobile-background-color)"
+              : "inherit",
+          }}
+        >
+          {/* <!-- Navigation links (hidden by default) --> */}
+          <div id="navLinks">
+            <a href={`${landing}`}>SB Hacks VIII</a>
+            <a href={`${intro}`}>About Us</a>
+            <a href={`${sponsors}`}>Sponsors</a>
+            {/* <a href={`${faq}`}>FAQ</a>
                     <a href={`${team}`}>TEAM</a> */}
-                    <a href={`${devpost}`} target="_blank" rel="noopener noreferrer">Devpost</a>
-                    {/* <Link href={`${landingPageLink}${volunteer}`}>VOLUNTEER</Link>
+            <a href={`${devpost}`} target="_blank" rel="noopener noreferrer">
+              Devpost
+            </a>
+            {/* <Link href={`${landingPageLink}${volunteer}`}>VOLUNTEER</Link>
                     <Link href={`${landingPageLink}${pastSite}`}>SB HACKS VII</Link> */}
-                </div>
-                {/* <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links --> */}
-                <a href="javascript:void(0);" className="icon" onClick={toggleMobNavBar}>
-                    {
-                        open ?
-                            <img className='mobileIcon' src={Close}/> :
-                            <img className='mobileIcon' src={HamburgerIcon}/>
-                    }
-                    
-                </a>
-            </div>
+          </div>
+          {/* <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links --> */}
+          <a
+            href="javascript:void(0);"
+            className="icon"
+            onClick={toggleMobNavBar}
+          >
+            {open ? (
+              <img className="mobileIcon" src={Close} />
+            ) : (
+              <img className="mobileIcon" src={HamburgerIcon} />
+            )}
+          </a>
+        </div>
 
-            <div id="navBar">
-                <ul>
-                    {/* <li><Link className='last' href={`${landingPageLink}${pastSite}`}>SB HACKS VII</Link></li>
+        <div id="navBar">
+          <ul>
+            {/* <li><Link className='last' href={`${landingPageLink}${pastSite}`}>SB HACKS VII</Link></li>
                     <li><Link href={`${landingPageLink}${volunteer}`}>VOLUNTEER</Link></li>
                     <li><Link href={`${landingPageLink}${mentors}`}>MENTOR</Link></li> */}
-                    <li><a className='last' href={`${devpost}`} target="_blank" rel="noopener noreferrer">Devpost</a></li>
-                    <li><a href={`${sponsors}`}>Sponsors</a></li>
-                    {/* <li><a href={`${team}`}>TEAM</a></li> */}
-                    {/* <li><a href={`${faq}`}>FAQ</a></li> */}
-                    <li><a href={`${intro}`}>About Us</a></li>
-                    <li><a href={`${landing}`}>SB Hacks VIII</a></li>
-                </ul>
-            </div>
-
+            <li>
+              <a
+                className="last"
+                href={`${devpost}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Devpost
+              </a>
+            </li>
+            <li>
+              <a href={`${sponsors}`}>Sponsors</a>
+            </li>
+            {/* <li><a href={`${team}`}>TEAM</a></li> */}
+            {/* <li><a href={`${faq}`}>FAQ</a></li> */}
+            <li>
+              <a href={`${intro}`}>About Us</a>
+            </li>
+            <li>
+              <a href={`${landing}`}>SB Hacks VIII</a>
+            </li>
+          </ul>
         </div>
+      </div>
     );
-}
+  }
+};
 
 export default NavBar;
