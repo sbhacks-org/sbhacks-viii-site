adddf5965536de1f174f814e004cfd74d89fa74c
Kevin Zhang <kevinzhang425@gmail.com>
2021-11-19 13:35:48 -0800

    Added otter to dashboard. SPlit dashboard into 2 columns

--- sbhacks-viii-frontend/src/components/Dashboard.js
+++ sbhacks-viii-frontend/src/components/Dashboard.js
@@ -14,6 +14,7 @@ import {
 } from "@material-ui/core";
 
 import Background from "../assets/backgrounds/tileable_background.jpg";
+import Otter from "../assets/images/otter.svg";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -27,17 +28,58 @@ const useStyles = makeStyles((theme) => ({
     left: "50%",
     transform: "translate(-50%,-50%)",
     color: "#365877",
+    display: "grid",
+    gridTemplateColumns: "15% 85%",
+    gridTemplateRows: "100%",
+    height: "80%",
     [theme.breakpoints.up("xl")]: {
-      width: "800px",
+      width: "1265px",
     },
     [theme.breakpoints.between("md", "lg")]: {
-      width: "600px",
+      width: "950px",
     },
     [theme.breakpoints.down("sm")]: {
       width: "80vw",
     },
     background: "#EEFFFF",
     borderRadius: "50px",
+  },
+  left: {
+    position: "relative",
+    gridColumnStart: "1",
+    gridColumnEnd: "2",
+  },
+  border: {
+    position: "absolute",
+    top: "3.8%",
+    right: "0px",
+    backgroundColor: "#C6E9F4",
+    width: "2px",
+    height: "92.4%",
+  },
+  right :{
+    gridColumnStart: "2",
+    gridColumnEnd: "3",
+  },
+  otterPic: {
+    marginTop: "10%",
+    [theme.breakpoints.up("xl")]: {
+      width: "120px",
+    },
+    [theme.breakpoints.between("md", "lg")]: {
+      width: "80px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "5vw",
+    },
+  },
+  fname: {
+    fontFamily: "NexaBold",
+    fontSize: "18px",
+  },
+  lname: {
+    fontFamily: "NexaLight",
+    fontSize: "18px",
   },
   submitBtn: {
     border: "none",
@@ -60,6 +102,7 @@ const useStyles = makeStyles((theme) => ({
 const Dashboard = () => {
   const [appStatus, setAppStatus] = useState("");
   const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
 
   const classes = useStyles();
 
@@ -92,6 +135,7 @@ const Dashboard = () => {
 
             setAppStatus(res.data.status);
             setFname(res.data.fname);
+            setLname(res.data.lname);
           })
           .catch((err) => {
             console.log("Error in getting user data: " + err);
@@ -111,14 +155,23 @@ const Dashboard = () => {
   return (
     <div className={classes.container}>
       <div className={classes.formContainer}>
-        <h2> Dashboard </h2>
-        Hi {fname}, your application status is
+        <div className={classes.left}>
+          <img className={`${classes.otterPic}`} src={Otter}/>
+          <div className={classes.fname}>{fname}</div>
+          <div className={classes.lname}>{lname}</div>
+          <div className={classes.border}></div>
+        </div>
+        <div className={classes.right}>
+          <h2> Dashboard </h2>
+          Hi {fname}, your application status is
 
-        <h1> {appStatus} </h1>
+          <h1> {appStatus} </h1>
 
-        <button type="submit" className={`${classes.submitBtn} clickable`} onClick={editApp}>
-        <Typography variant="subtitle1">Edit Application</Typography>
-        </button>
+          <button type="submit" className={`${classes.submitBtn} clickable`} onClick={editApp}>
+            <Typography variant="subtitle1">Edit Application</Typography>
+          </button>
+        </div>
+        
       </div>
     </div>
   );
