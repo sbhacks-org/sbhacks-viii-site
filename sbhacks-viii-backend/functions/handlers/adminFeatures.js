0a33b62b09b3ed1a5f392c63d79e40dcd7332c41
dguo <dguo@umail.ucsb.edu>
2021-12-20 22:49:01 -0800

    added filtering backend

--- sbhacks-viii-backend/functions/handlers/adminFeatures.js
+++ sbhacks-viii-backend/functions/handlers/adminFeatures.js
@@ -46,7 +46,9 @@ exports.getFilteredEmails = async (req, res) => {
       doc.data().universityName,
     ]);
 
-    const hackerEmailAddresses = emailAddresses.filter(email => email[2] === "hacker");
+    const hackerEmailAddresses = emailAddresses.filter(
+      (email) => email[2] === "hacker"
+    );
 
     const completeApplications = [];
     for (const email of hackerEmailAddresses) {
@@ -71,14 +73,15 @@ exports.getFilteredEmails = async (req, res) => {
       }
       if (email[3] in completeApplicationsBySchool) {
         completeApplicationsBySchool[email[3]].push(email[0]);
-      }
-      else {
+      } else {
         completeApplicationsBySchool[email[3]] = [email[0]];
       }
     }
 
     const completeApplicationsBySchoolCounts = {};
-    for (const [school, emails] of Object.entries(completeApplicationsBySchool)) {
+    for (const [school, emails] of Object.entries(
+      completeApplicationsBySchool
+    )) {
       completeApplicationsBySchoolCounts[school] = emails.length;
     }
 
@@ -89,14 +92,15 @@ exports.getFilteredEmails = async (req, res) => {
       }
       if (email[3] in incompleteApplicationsBySchool) {
         incompleteApplicationsBySchool[email[3]].push(email[0]);
-      }
-      else {
+      } else {
         incompleteApplicationsBySchool[email[3]] = [email[0]];
       }
     }
 
     const incompleteApplicationsBySchoolCounts = {};
-    for (const [school, emails] of Object.entries(incompleteApplicationsBySchool)) {
+    for (const [school, emails] of Object.entries(
+      incompleteApplicationsBySchool
+    )) {
       incompleteApplicationsBySchoolCounts[school] = emails.length;
     }
 
@@ -108,9 +112,46 @@ exports.getFilteredEmails = async (req, res) => {
       completeApplicationsBySchool: completeApplicationsBySchool,
       incompleteApplicationsBySchool: incompleteApplicationsBySchool,
       completeApplicationsBySchoolCounts: completeApplicationsBySchoolCounts,
-      incompleteApplicationsBySchoolCounts: incompleteApplicationsBySchoolCounts,
+      incompleteApplicationsBySchoolCounts:
+        incompleteApplicationsBySchoolCounts,
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: `something went wrong: ${err}` });
+  }
+};
 
+exports.getFilteredEmails2 = async (req, res) => {
+  try {
+    const { authToken, filters_dict } = req.query;
+    // filters should be a dictionary of key, value
+    // key: application field
+    // value: list of acceptable values
+    let filters = filters_dict;
+    if (filters === undefined) {
+      filters = {};
+    }
+
+    if (authToken != process.env.ADMIN_AUTH_TOKEN) {
+      res.status(400).json({ error: "invalid admin auth token" });
+      return;
+    }
+
+    const snapshot = await db.collection("hackers").get();
+    let emailAddresses = snapshot.docs.map((doc) => doc.data());
+
+    for (const key in Object.keys(filters)) {
+      emailAddresses = emailAddresses.filter(
+        (email) => email.key in filters.key
+      );
+    }
+
+    const filteredEmails = emailAddresses.map((hacker) => hacker.emailAddress);
+
+    res.json({
+      filteredEmails: filteredEmails,
+      filteredEmailsCSV: filteredEmails.join(", "),
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: `something went wrong: ${err}` });
