293a5a4c56e46b96033ec1627c5994489311fbad
dguo <dguo@umail.ucsb.edu>
2022-01-18 05:04:11 -0800

    deployed changes

--- sbhacks-viii-frontend/src/components/MailingList.js
+++ sbhacks-viii-frontend/src/components/MailingList.js
@@ -119,7 +119,7 @@ function MailingList() {
         }
         <div className='eventDateContainer'>
           <div className='eventDate'>
-            <div className='text small'>January 28-30, 2022</div>
+            <div className='text small'> February 4-6, 2022</div>
             <div className='text large'>Corwin Pavilion, UCSB</div>
           </div>
         </div>
