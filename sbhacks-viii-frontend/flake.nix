0ea8dfa74bc6b949c4efdf574841b3d9c6a9ea6f
Youwen Wu <youwenw@gmail.com>
2025-10-02 01:21:31 -0700

    feat: switch to pnpm and bump dependencies, add flake that builds

---
+++ sbhacks-viii-frontend/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "flake for building sb hacks 2022 website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              nodejs_20
+              pnpm
+            ];
+
+            NODE_OPTIONS = "--openssl-legacy-provider";
+          };
+        }
+      );
+    };
+}
